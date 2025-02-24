cf7c31a17eab11d65bfc1d0d0c9bc1469b0362d9
Cartic Natraj <cnatraj@gmail.com>
2025-02-24 11:45:59 -0800

    added google analytics

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,7 +8,20 @@
 import vuetify from "./vuetify";
 import pinia from "./pinia";
 import router from "@/router";
+import VueGtag from "vue-gtag"; //Google Analytics
 
 export function registerPlugins(app) {
-  app.use(vuetify).use(pinia).use(router);
+  app
+    .use(vuetify)
+    .use(pinia)
+    .use(router)
+    .use(
+      VueGtag,
+      {
+        config: {
+          id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID,
+        },
+      },
+      router
+    );
 }
